8f306e112a035e3c8d6d144767a7d10c60054fde
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2026-04-16 18:08:21 +0200

    Update shared configs

--- .ee-bench/codegen/eval/run.sh
+++ .ee-bench/codegen/eval/run.sh
@@ -22,8 +22,8 @@ _run_tests() {
   mkdir -p "$ARTIFACTS_DIR"
 
   set +e
-  dotnet test {{ instance.test_framework_flag }} "{{ instance.test_project }}" \
-    --logger "{{ instance.test_logger }}" {{ instance.build_flags }} \
+  dotnet test --framework net5.0 "./tests/BenchmarkDotNet.Tests/BenchmarkDotNet.Tests.csproj" \
+    --logger "junit;LogFilePath=$ARTIFACTS_DIR/tests.log" -p:TreatWarningsAsErrors=false \
     > "/tmp/${label}_stdout.log" 2> "/tmp/${label}_stderr.log"
   set -e
 
